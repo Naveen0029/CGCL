da719bee8bb49ef88329646ee2ab5c25303a6c3b
Naveen Bishnoi <naveendharnia0029@gmail.com>
2021-05-06 00:48:22 +0530

    upto 114

---
+++ Dom/app.js
@@ -0,0 +1,3 @@
+//document.getElementsByTagName("ab").style.display='none';
+document.querySelector('.navbar-brand').style.display = 'none';
+document.querySelector('.navbar-nav').style.textAlign='left';
