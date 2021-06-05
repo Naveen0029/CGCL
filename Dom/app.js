dcfe21d88de8a7f077caa5012b6087bf738da231
Naveen Bishnoi <naveendharnia0029@gmail.com>
2021-06-05 14:55:32 +0530

    deploy2

--- Dom/app.js
+++ Dom/app.js
@@ -63,12 +63,22 @@ search.insertAdjacentHTML('beforeend',text);
 var tit=document.getElementsByTagName('TITLE');*///not working this method
 var linki1=document.createElement('link');
 var linki2=document.createElement('link');
+var linki3=document.createElement('link');
 linki1.rel="stylesheet";
 linki1.href="node_modules/font-awesome/css/font-awesome.min.css";
 linki2.rel="stylesheet";
 linki2.href="node_modules/bootstrap-social/bootstrap-social.css";
+linki3.href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css";
+linki3.rel="stylesheet";
+linki3.integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x";
 document.body.appendChild(linki1);
-document.body.appendChild(linki2);//dont know how it is working
+document.body.appendChild(linki2);
+document.body.appendChild(linki3);
+var js1=document.createElement('script');
+js1.src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js";
+js1.integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4";
+js1.crossorigin="anonymous";
+document.body.appendChild(js1);
 
 //implementing search bar
 /*const searchBar=document.getElementById("example-search-input");
